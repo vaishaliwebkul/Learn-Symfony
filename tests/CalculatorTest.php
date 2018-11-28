e484e74d2215bea29f068e0740741290c426608b
vaishali agarwal <vaishaliagarwal.tester918@webkul.com>
2018-11-28 18:11:37 +0530

    update travis

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -13,7 +13,7 @@ class CalculatorTest extends TestCase
         $result = $calculator->add(30, 12);
 
         // assert that your calculator added the numbers correctly!
-        $this->assertEquals(42, $result);
+        $this->assertEquals(62, $result);
     }
 
 
